a0914fad3f53cdd3dcb397631d119a8f0ffe44ab
Christine Tham <chris.tham@hellotham.com>
2026-04-24 15:43:19 +1000

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      tsconfigPaths: true,
+    },
   },
   site: 'https://portfolio.christham.net',
 })
